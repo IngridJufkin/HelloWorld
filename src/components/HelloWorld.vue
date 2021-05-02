1a4653fa6d573ee92188c227602e523a250e9104
IngridJufkin <ingridjufkin@gmail.com>
2021-05-02 21:30:32 +0300

    Mounted trial

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,9 +3,10 @@
 <div class="bg-white shadow rounded px-3 pt-3 pb-5 border border-white">
 
     <div>
-      <div>
+      <h2> Pick a date </h2>
+     <div>
         <input
-        v-model="year"
+        v-model="input.year"
          placeholder ="Year"
          min="2000"
          max="2030"
@@ -16,7 +17,7 @@
 
       <div>
         <input
-        v-model="month"
+        v-model="input.month"
          placeholder ="Month"
          min="1"
          max="12"
@@ -27,7 +28,7 @@
       
       <div>
         <input
-        v-model="day"
+        v-model="input.day"
          placeholder ="Day"
          type="number"
          min="1"
@@ -37,6 +38,7 @@
     </div>
     <br>
 
+    {{input.month}}
     <div class="row mt-5">
       <div class="col text-right">
         <button
@@ -97,15 +99,28 @@ export default {
     }
     
   },
+
   props: {
     msg: String
   },
 
+  computed: {
+    datedata() {
+      return dateformat(
+        this.input.year,
+        this.input.month,
+        this.input.day,
+        this.input.date
+      )
+    },
+  },
+
   methods: {
     dateformat() {
-    let day = this.day;
-    let month = this.month;
-    let year = this.year;
+    let day = this.input.day;
+    let month = this.input.month;
+    let year = this.input.year;
+
     let date = year + "/" + month + "/" + day
     if (month < 10) {
           date = year + "/0" + month + "/" + day
@@ -116,20 +131,23 @@ export default {
     if (month < 10 && day < 10) {
           date = year + "/0" + month + "/0" + day
     }
-    this.date = date
-    console.log("This date 2: " + this.date)
+    this.input.date = date
+  console.log(date)
       }
     },
   
-mounted () {
+mounted() {
+  let date = this.input.date
+  console.log("LOGIN " + date)
+
     axios
-      .get(`https://en.wikipedia.org/api/rest_v1/feed/featured/${this.date}`)
+      //.get(`https://en.wikipedia.org/api/rest_v1/feed/featured/${this.input.date}`)
+      .get(`https://en.wikipedia.org/api/rest_v1/feed/featured/2021/04/30`)
       .then(response => {
         console.log (response.data)
         this.items = response.data.mostread.articles
          this.thumbnail = response.data.mostread.articles.thumbnail
-         console.log("olen siin")
-         console.log(this.thumbnail)
+         console.log("olen siin" + this.input.date)
       })
       .catch(error => {
         console.log(error)
@@ -143,6 +161,10 @@ mounted () {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+h2 {
+  margin: 10px 0px;
+}
 
 h3 {
   margin: 40px 0 0;
